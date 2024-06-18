c27e3e0b3462bb6250f116501f11caacbd66ab91
Rian Lucas <rianlucas2014@gmail.com>
2024-06-18 14:20:32 -0300

    fix: axios interceptor

--- src/api/server.js
+++ src/api/server.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
-export const http = axios.create({ baseURL: "https://acha-se-back-end.onrender.com", headers: {
-  Authorization: "Bearer " + getToken()
-}})
+export const http = axios.create({ baseURL: "https://acha-se-back-end.onrender.com"})
+
+http.interceptors.request.use(
+  (config) => {
+    const token = getToken()
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // console.log("request error", error);
+    return Promise.reject(error);
+  }
+);
 
 function getToken(){
   const token = localStorage.getItem("token")
+
   if(token){
     return token
   }
